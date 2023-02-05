8caa711ff27338b38b6292bf8738402422b46978
mishadiatel <mishadiatel13@gmail.com>
2023-02-05 22:58:24 +0200

    React 04 added Fragment to App.js

--- React/04 - adding user application/src/App.js
+++ React/04 - adding user application/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './cmponents/Users/AddUser';
 import UsersList from './cmponents/Users/UsersList';
@@ -15,10 +15,10 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
