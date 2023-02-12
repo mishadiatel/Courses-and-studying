01110ee1c2b00ace08526184040409f8db6c76fb
mishadiatel <mishadiatel13@gmail.com>
2023-02-13 00:46:53 +0200

    React 06 added logic for adding new elements to cart and displaying them

--- React/06 - food order app/src/App.js
+++ React/06 - food order app/src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 
 function App() {
@@ -18,13 +19,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShowm && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
