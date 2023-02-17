1114528f840127167292ab43f26bad62cd753bd3
mishadiatel <mishadiatel13@gmail.com>
2023-02-17 17:38:49 +0200

    React 09 added AddMovie component

--- React/09 - working with API/src/App.js
+++ React/09 - working with API/src/App.js
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -10,12 +11,11 @@ function App() {
 
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
-    setError(null)
+    setError(null);
     try {
       const response = await fetch('https://swapi.dev/api/films/');
-
       if (!response.ok) {
-        throw new Error('Somwthing went wrong');
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
@@ -29,19 +29,21 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-
   }, []);
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  let content = <p>Found no movies</p>;
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
@@ -57,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
