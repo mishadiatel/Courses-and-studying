bc86177423c933340e186fedbb1c4efcf5271336
mishadiatel <mishadiatel13@gmail.com>
2023-02-23 23:19:11 +0200

    React 15 added react router, some pages and error page

--- React/15 - react router/src/App.js
+++ React/15 - react router/src/App.js
@@ -1,5 +1,33 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route >
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ]
+  },
+
+]);
+
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ React/15 - react router/src/pages/Products.js
@@ -0,0 +1,7 @@
+
+
+const ProductsPage = (props) => {
+    return <h1>The products page</h1>
+}
+
+export default ProductsPage;
