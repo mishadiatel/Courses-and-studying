2aabe86ce685c0011c50d6fafaaf29c21c85dbb3
mishadiatel <mishadiatel13@gmail.com>
2023-03-09 22:07:03 +0200

    React 23 added simple unit tests

---
+++ React/23 - testing react apps/src/components/Async.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react"
+import Async from "./Async"
+
+describe('Async component', () => {
+    test('renders posts if request successed', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'First post'}]
+        });
+        render(<Async />);
+
+        const listItemsElements = await screen.findAllByRole("listitem");
+        expect(listItemsElements).not.toHaveLength(0);
+    })
+})
